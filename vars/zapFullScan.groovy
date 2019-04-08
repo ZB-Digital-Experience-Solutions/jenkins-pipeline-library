5cb73755cb73596db560e81a25a08b6d2115ed53
bhargav <bhargavdev9898@gmail.com>
2019-04-08 17:43:53 +0000

    Merge branch 'zap-testing' of gitlab.com:sirius-aag-public/jenkins-pipeline-library into zap-testing

---
+++ vars/zapFullScan.groovy
@@ -0,0 +1,26 @@
+#!/usr/bin/groovy
+def call( serviceName, DNS_NAME) {
+
+    try {
+
+        container('owasp-zap') {
+            sh """
+                  mkdir /zap/wrk
+                  (/zap/zap-full-scan.py -r zapreport.html -t http://${serviceName}.${DNS_NAME} || true) >cat.txt
+                  cp /zap/wrk/zapreport.html .
+                """
+              def zaptest = sh(returnStdout: true, script: 'cat cat.txt').trim()
+              print "${zaptest}"
+
+              def zaptestresult = sh(returnStdout: true, script: 'cat cat.txt | tail -1').trim()
+              print "${zaptestresult}"
+
+              sh "rm cat.txt"
+              return zaptestresult
+            }
+
+    } catch (err) {
+        echo "Exception: ${err}"
+        throw err
+    }
+}
